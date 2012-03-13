4ad6379c362d699278d16985d7aca956720a554d
Lukasz Piliszczuk <lukasz.pili@gmail.com>
2012-03-13 14:47:11 +0100

    Initial commit.

---
+++ src/com/siu/bicyclette/activity/ApplicationActivity.java
@@ -0,0 +1,16 @@
+package com.siu.bicyclette.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.siu.bicyclette.R;
+
+public class ApplicationActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
